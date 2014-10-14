c286cded3c72fd4007543bad35e0cb016944efa8
asmol <saa.fkn@yandex.ru>
2014-10-14 20:36:59 +0400

    Indentation fixed

--- random.c
+++ random.c
@@ -6,17 +6,17 @@
 
 int main(int argc, char** argv)
 {
-	int count;
-	if (argc != 3 || (count = strtol(argv[2],NULL,10)) < MIN_NUMBERS || count > MAX_NUMBERS)
-	{
-		printf("./random [file] [numbers count (from %d to %d)]\n",MIN_NUMBERS,MAX_NUMBERS);
-		return 1;
-	}
-	FILE* file = fopen(argv[1],"w");
-	srand(time(NULL));
-	int i;
-	for (i = 0; i < count; i++)
-		fprintf(file,"%d ",rand() % 999 + 1);
-	fclose(file);
-	return 0;
+    int count;
+    if (argc != 3 || (count = strtol(argv[2],NULL,10)) < MIN_NUMBERS || count > MAX_NUMBERS)
+    {
+        printf("./random [file] [numbers count (from %d to %d)]\n",MIN_NUMBERS,MAX_NUMBERS);
+        return 1;
+    }
+    FILE* file = fopen(argv[1],"w");
+    srand(time(NULL));
+    int i;
+    for (i = 0; i < count; i++)
+        fprintf(file,"%d ",rand() % 999 + 1);
+    fclose(file);
+    return 0;
 }
